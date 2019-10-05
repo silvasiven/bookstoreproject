7156bff22430a9a4df0c39bf2d38f11a9ca90800
unknown <silvasiven@gmail.com>
2019-10-05 13:12:56 +0300

    securing web application II

--- bookstore/src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -12,6 +12,8 @@ import hh.swd20.bookstore.domain.Book;
 import hh.swd20.bookstore.domain.BookRepository;
 import hh.swd20.bookstore.domain.Category;
 import hh.swd20.bookstore.domain.CategoryRepository;
+import hh.swd20.bookstore.domain.User;
+import hh.swd20.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -24,7 +26,7 @@ public class BookstoreApplication {
 
 
 @Bean
-	public CommandLineRunner bookDemo(BookRepository bookRepository, CategoryRepository categoryRepository) { 
+	public CommandLineRunner bookDemo(BookRepository bookRepository, CategoryRepository categoryRepository,UserRepository userRepository) { 
 	return (args) -> {
 		log.info("save a couple of books");
 		categoryRepository.save(new Category("Horror"));
@@ -34,6 +36,11 @@ public class BookstoreApplication {
 		bookRepository.save(new Book("Hauskimmat vitsit","Kari Kirjailija","55016",2017,40, categoryRepository.findByName("Horror").get(0)));
 		bookRepository.save(new Book("Anna mun arvata","Oona Kalervo","690582",2017,46, categoryRepository.findByName("Biography").get(0)));	
 		bookRepository.save(new Book("Sininen lintu","Oona Kalervo","690582",2007,76, categoryRepository.findByName("Fiction").get(0)));	
+		
+		User user1 = new User("user", "$2a$10$/iEyHBLqguFtM/aIiaOKmu2PiPe635mDmoqkOdG21yJbHGLgYF07e", "kayttaja@kayttaja.fi", "USER");
+		User user2 = new User("admin", "$2a$10$JWXmJcPJWkMuoPWgypbkou492xYY.8IGhmnd91PvJe0FPyz.dxFLK", "admin@admin.fi", "ADMIN");
+		userRepository.save(user1);
+		userRepository.save(user2);
 		
 		log.info("fetch all books");
 		for (Book book : bookRepository.findAll()) {
